9ccb803082c1dd4d8fc3511e572c677f2b91a7e1
ArfatulAsif <awakicde@gmail.com>
2025-01-14 01:28:29 +0600

    fix tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {  RouterProvider } from 'react-router-dom';
 import router from './routes/Router';
-import './index.css'
 
 
 createRoot(document.getElementById('root')).render(
