1c32d39b5a670f77781ad52ccf57753c8205657e
ArfatulAsif <awakicde@gmail.com>
2025-01-14 01:29:13 +0600

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import router from './routes/Router';
-import './index.css'
 
 
 createRoot(document.getElementById('root')).render(
