f120bc1045a21e759beae6193749a11c515bfe21
Mutah3r <evil.programmer.stephen@gmail.com>
2025-01-15 22:27:08 +0600

    add static image serving

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./db');  // Import database connection
-const routes = require('./routes/userRoute');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./db"); // Import database connection
+const routes = require("./routes/userRoute");
 
 const app = express();
 app.use(cors());
@@ -15,13 +15,15 @@ const PORT = process.env.PORT || 8000;
 // Connect to MongoDB
 connectDB();
 
-app.use('/auth', routes);
-app.use('/product', routes);
-app.use('/buy', routes);
-app.use('/bank', routes);
-app.use('/supply', routes);
-app.use('/get', routes);
+// Serve static images
+app.use("/images", express.static("images"));
 
+app.use("/auth", routes);
+app.use("/product", routes);
+app.use("/buy", routes);
+app.use("/bank", routes);
+app.use("/supply", routes);
+app.use("/get", routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
