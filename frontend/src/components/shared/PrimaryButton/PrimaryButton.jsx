8152021f589fc4a45faf392703cb197fa413955b
Mutah3r <evil.programmer.stephen@gmail.com>
2025-01-16 02:24:29 +0600

    design single product page

--- frontend/src/components/shared/PrimaryButton/PrimaryButton.jsx
+++ frontend/src/components/shared/PrimaryButton/PrimaryButton.jsx
@@ -2,7 +2,7 @@ const PrimaryButton = ({ title, icon, classNames, loading }) => {
   return (
     <button
       disabled={loading}
-      className={`group flex justify-center items-center gap-3 bg-orange-500 px-4 text-md py-2 text-[#F6F6F6] rounded-md hover:bg-orange-50 border border-orange-600 hover:text-orange-500 transition-all duratoin-150 ${classNames}`}
+      className={`group flex justify-center items-center gap-3 bg-orange-500 px-4 text-md py-2 text-[#F6F6F6] rounded-md hover:bg-orange-50 border border-orange-600 hover:text-orange-500 transition-all duration-150 ${classNames}`}
     >
       {loading && (
         <span className="loading loading-spinner text-neutral"></span>
